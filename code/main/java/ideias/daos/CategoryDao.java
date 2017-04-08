1eed4f7186a915b40285df59343df6159352208e
pereiraJr <pereiraJr>
2017-04-08 00:38:47 -0300

    Src

---
+++ code/main/java/ideias/daos/CategoryDao.java
@@ -0,0 +1,46 @@
+package ideias.daos;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.inject.Inject;
+import ideias.models.PaginatedList;
+
+import ideias.models.Category;
+
+public class CategoryDao
+{
+
+   @Inject
+   private EntityManager manager;
+
+   public List<Category> all()
+   {
+      return manager.createQuery("select c from Category c", Category.class).getResultList();
+   }
+
+   public void save(Category category)
+   {
+      manager.persist(category);
+   }
+
+   public Category findById(Integer id)
+   {
+      return manager.find(Category.class, id);
+   }
+
+   public void remove(Category category)
+   {
+      manager.remove(category);
+   }
+
+   public void update(Category category)
+   {
+      manager.merge(category);
+   }
+
+   public PaginatedList paginated(int page, int max)
+   {
+      return new PaginatorQueryHelper().list(manager, Category.class, page, max);
+   }
+}
